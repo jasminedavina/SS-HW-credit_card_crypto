4e382e4a40818eeece45b1f58938988f6f38b9b9
Jasmine Davina <jasminedavina@gmail.com>
2026-03-17 09:45:49 +0800

    fix rubocop offenses

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# module for implementing cc with luhn algorithm
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -10,12 +13,12 @@ module LuhnValidator
     # exlcude the ast digit (payload)
     nums_a_reversed.each_with_index do |x, i|
       # for the odd position after reversed
-      if i.odd? 
-        nums_a_reversed[i] = x * 2 
+      if i.odd?
+        nums_a_reversed[i] = x * 2
         nums_a_reversed[i] -= 9 if nums_a_reversed[i] > 9
       end
     end
     sum_nums_a = nums_a_reversed.sum
-    sum_nums_a % 10 == 0
+    (sum_nums_a % 10).zero?
   end
 end
